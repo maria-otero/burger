4d5190f862fa71635e6c73481fc5c7f65a34afac
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-06-08 01:08:56 -0700

    add more style. CRUD working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 USE burgers_db;
 
-INSERT INTO burgers (name) VALUES ('Meat - Bacon');
-INSERT INTO burgers (name) VALUES ('Meat - Tomato');
-INSERT INTO burgers (name) VALUES ('Mexican Burger');
-INSERT INTO burgers (name) VALUES ('Chilean Burger');
-INSERT INTO burgers (name) VALUES ('American Burger');
+INSERT INTO burgers (name) VALUES ('Meat + Bacon');
+INSERT INTO burgers (name) VALUES ('Meat + Tomato');
+INSERT INTO burgers (name) VALUES ('Mexican Style');
+INSERT INTO burgers (name) VALUES ('Chilean Mix');
+INSERT INTO burgers (name) VALUES ('American');
 INSERT INTO burgers (name, devoured) VALUES ('Vegan', false);
-INSERT INTO burgers (name, devoured) VALUES ('Chicken - Bacon', false);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken + Bacon', false);
 INSERT INTO burgers (name, devoured) VALUES ('Cheeses + cheese', false);
