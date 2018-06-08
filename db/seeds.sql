4294334335a8b4d70a15d8991f3bbb0154abd8f5
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-06-07 23:32:49 -0700

    names diplay for each burger

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,10 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ('Meat - Bacon');
-INSERT INTO burgers (burger_name) VALUES ('Meat - Tomato');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken - Bacon', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeses', true);
-INSERT INTO burgers (burger_name) VALUES ('Mexican Burger');
+INSERT INTO burgers (name) VALUES ('Meat - Bacon');
+INSERT INTO burgers (name) VALUES ('Meat - Tomato');
+INSERT INTO burgers (name) VALUES ('Mexican Burger');
+INSERT INTO burgers (name) VALUES ('Chilean Burger');
+INSERT INTO burgers (name) VALUES ('American Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Vegan', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken - Bacon', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeses', true);
