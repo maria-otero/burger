88fcee2730598f9397dd7239c59d5b3421c04621
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-06-07 20:44:32 -0700

    db created, deplayed and function delete running

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Meat - Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Meat - Tomato');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken - Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeses', true);
+INSERT INTO burgers (burger_name) VALUES ('Mexican Burger');
